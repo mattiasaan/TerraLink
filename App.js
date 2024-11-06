9f3f812a6e26857fc4e357a207f728ea42d40ce4
mattiasaan <mattiasan0408@gmail.com>
2024-11-06 18:16:59 +0100

    bestemmio

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
 import FarmacieScreen from './Screens/Secondari/Farmacie';
+import MappaScreen from './Screens/Secondari/Mappa';
 import AutobusScreen from './Screens/Secondari/Bus';
-import MappaScreen from './Screens/Secondari/Mappa'
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Farmacie" component={FarmacieScreen} />
-        <Stack.Screen name="Mappa" component={MappaScreen} />
-        <Stack.Screen name="Autobus" component={AutobusScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Farmacie" component={FarmacieScreen} />
+          <Stack.Screen name="Mappa" component={MappaScreen} />
+          <Stack.Screen name="Autobus" component={AutobusScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
--- Screens/Secondari/Bus.js
+++ Screens/Secondari/Bus.js
@@ -4,36 +4,41 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const AutobusScreen = () => {
     return(
-        <ScrollView >
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.TouchArea}>
-                <Text>Linea 3</Text>
-            </TouchableOpacity>
+        <ScrollView style={styles.container}>
+            <View style={styles.TouchArea}>
+                <TouchableOpacity>
+                    <Text>Linea 3</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.TouchArea}>
+                <TouchableOpacity>
+                    <Text>Linea 3</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.TouchArea}>
+                <TouchableOpacity>
+                    <Text>Linea 3</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.TouchArea}>
+                <TouchableOpacity>
+                    <Text>Linea 3</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.TouchArea}>
+                <TouchableOpacity>
+                    <Text>Linea 3</Text>
+                </TouchableOpacity>
+            </View>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    Container:{
+    container: {
         flex: 1,
-        padding: 10
+        padding: 10,
+        backgroundColor: "#ffffff"
     },
     TouchArea:{
         backgroundColor: 'white',
